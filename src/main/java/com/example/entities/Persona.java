73211b70594e055e09f54700cba27aa8fceb5440
clemmory <clementine.mory@gmail.com>
2024-02-14 13:01:32 +0100

    Entity Persona

---
+++ src/main/java/com/example/entities/Persona.java
@@ -0,0 +1,32 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String primerApellido;
+    private String segundoApellido;
+
+
+}
